11642b4855e609b2a33d1c723ce5158b38d0c931
johnsijoy <johnsijoy9@gmail.com>
2024-12-16 09:23:38 -0800

    completed

--- src/server.js
+++ src/server.js
@@ -12,8 +12,7 @@ app.use(cors());
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+  
 }).then(() => console.log('MongoDB Connected Successfully'))
   .catch((err) => console.log(err));
 
